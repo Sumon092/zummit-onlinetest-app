4593b45fb9ffdb341236154c76712a1587e85ec4
Sumon <m.sumon@outlook.com>
2023-01-01 17:15:09 +0600

    netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import './App.css';
+import Navbar from './container/Navbar';
+import Student from "./container/Student";
+import Teacher from "./container/Teacher";
+import Home from "./container/Home";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter><Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Student" element={<Student />} />
+          <Route path="/Teacher" element={<Teacher />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
